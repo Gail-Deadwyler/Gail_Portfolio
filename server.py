43213873605d24b35f0305d00c440c66c3842b2d
Gail-Deadwyler <webwyler@yahoo.com>
2020-05-12 22:09:48 -0500

    add routes
    modified html pages, added base.html

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def about(page_name):
+    return render_template(page_name)
+
+# if this page is the main page, run it
+if __name__ == '__main__':
+    app.run(debug=True)
